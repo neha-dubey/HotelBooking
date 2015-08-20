52b01e44478c0af2226b027d803303964692eabe
neha <neha24.7@rediffmail.com>
2015-08-19 23:53:34 -0400

    added features for displaying hotel choices

--- HotelBooking/src/com/hotelBooking/DisplayChoices.java
+++ HotelBooking/src/com/hotelBooking/DisplayChoices.java
@@ -6,20 +6,33 @@ public class DisplayChoices {
 		System.out.println("Preferences as entered are: " + rewardee + "\n" + weekday + "\n" + starPref + "\n" + hotelPref);
 		
 		if (starPref.equals("") && hotelPref.equals("")){
+			
 			System.out.println("No star preference and hotel preference provided");
-			//TODO
+			DisplayDefaultChoices disp = new DisplayDefaultChoices();
+			disp.DispDefChoices(rewardee, weekday);
+			
 		} else if (starPref.equals("5") | hotelPref.equals("Del")){
+			
 			System.out.println("Your preference matches Delta Hotels");
 			//TODO
+			
 		} else if (starPref.equals("4") | hotelPref.equals("Rit")) {
+			
 			System.out.println("Your preference matches Ritz-Carlton Hotel");
 			//TODO
+			
 		} else if (starPref.equals("3") | hotelPref.equals("Ren")){
+			
 			System.out.println("Your preference matches Renaissance Hotels");
 			//TODO
+			
 		} else {
+			
 			System.out.println("Sorry we don't have hotels available with preferences provided");
-			System.out.println("Please select again without preferences for hotel suggestions");
+			System.out.println("Following are the suggestions we have for you:");
+			DisplayDefaultChoices disp = new DisplayDefaultChoices();
+			disp.DispDefChoices(rewardee, weekday);
+			
 		}
 	}
 }
--- HotelBooking/src/com/hotelBooking/DisplayDefaultChoices.java
+++ HotelBooking/src/com/hotelBooking/DisplayDefaultChoices.java
@@ -2,8 +2,56 @@ package com.hotelBooking;
 
 public class DisplayDefaultChoices extends DisplayChoices {
 	
+	private int deltaPrice;
+	private int ritzPrice;
+	private int renaisPrice;
+
 	void DispDefChoices(boolean member, boolean weekday){
-		//TODO .
+		if (member == true){
+			if (weekday == true){
+				setDeltaPrice(1 * 210);
+				setRitzPrice(1 * 120);
+				setRenaisPrice(1 * 90);
+			}else {
+				setDeltaPrice(1 * 230);
+				setRitzPrice(1 * 180);
+				setRenaisPrice(1 * 95);
+			}
+		}else if (member == false){
+			if (weekday == true){
+				setDeltaPrice(1 * 250);
+				setRitzPrice(1 * 195);
+				setRenaisPrice(1 * 100);
+			}else {
+				setDeltaPrice(1 * 290);
+				setRitzPrice(1 * 250);
+				setRenaisPrice(1 * 120);
+			}
+		}
+	}
+
+	public int getDeltaPrice() {
+		return deltaPrice;
+	}
+
+	public void setDeltaPrice(int deltaPrice) {
+		this.deltaPrice = deltaPrice;
+	}
+
+	public int getRitzPrice() {
+		return ritzPrice;
+	}
+
+	public void setRitzPrice(int ritzPrice) {
+		this.ritzPrice = ritzPrice;
+	}
+
+	public int getRenaisPrice() {
+		return renaisPrice;
+	}
+
+	public void setRenaisPrice(int renaisPrice) {
+		this.renaisPrice = renaisPrice;
 	}
 
 }
